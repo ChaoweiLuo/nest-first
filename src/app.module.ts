488a58fbeda5c19a4b752702d8a1879ee902ecd7
chaowei <chaowei.luo@live.cn>
2023-02-01 12:19:46 +0800

    feat: config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config/dist';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BookController } from './book/book.controller';
 import { BookService } from './book/book.service';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [AppController, BookController],
   providers: [AppService, BookService],
 })
