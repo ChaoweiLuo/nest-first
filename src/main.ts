a65060533064344d6bd32bd9b9e184e0e8a16c57
chaowei <chaowei.luo@live.cn>
2023-02-01 17:29:48 +0800

    feat: openapi

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { ConfigService } from '@nestjs/config/dist';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+
+  const config = new DocumentBuilder()
+    .setTitle('Book example')
+    .setDescription('The books API description')
+    .setVersion('1.0')
+    .addTag('books')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   const port = configService.get<string>('PORT');
   await app.listen(port || 3001);
 }
