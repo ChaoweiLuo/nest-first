67db6d0294ac5f6ce15589305c495e7cec07e7c4
chaowei <chaowei.luo@live.cn>
2023-02-08 10:25:22 +0800

    feat: fetch axios

--- src/mysql/mysql.controller.ts
+++ src/mysql/mysql.controller.ts
@@ -1,11 +1,33 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UsePipes, ValidationPipe } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { MysqlService } from './mysql.service';
 import { CreateMysqlDto } from './dto/create-mysql.dto';
 import { UpdateMysqlDto } from './dto/update-mysql.dto';
+import { HttpService } from '@nestjs/axios';
+import { lastValueFrom } from 'rxjs';
 
 @Controller('mysql')
 export class MysqlController {
-  constructor(private readonly mysqlService: MysqlService) {}
+  constructor(
+    private readonly mysqlService: MysqlService,
+    private readonly httpService: HttpService,
+  ) {}
+
+  @Get('http')
+  async fetchBaidu() {
+    const res = this.httpService.get<string>('https://baidu.com');
+    const baidu = await lastValueFrom(res);
+    return baidu.data;
+  }
 
   @Post()
   // @UsePipes(new ValidationPipe())
--- src/mysql/mysql.module.ts
+++ src/mysql/mysql.module.ts
@@ -3,10 +3,11 @@ import { MysqlService } from './mysql.service';
 import { MysqlController } from './mysql.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User]), HttpModule ],
   controllers: [MysqlController],
-  providers: [MysqlService]
+  providers: [MysqlService],
 })
 export class MysqlModule {}
